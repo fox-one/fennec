9fe2be24f8aca1aeba1975dc0289620af646bfa4
zhangqingwu <18656007202@163.com>
2021-04-07 17:49:18 +0800

    fix: fix import guard

--- packages/mixin-extension-ui/src/components/guard/init.vue
+++ packages/mixin-extension-ui/src/components/guard/init.vue
@@ -2,6 +2,7 @@
   <v-container class="mb-5">
     <account-import-logo />
     <account-import-keystore class="mt-5" />
+    <account-import-with-provider class="mt-5" />
   </v-container>
 </template>
 
@@ -9,11 +10,13 @@
 import { Component, Vue } from "vue-property-decorator";
 import AccountImportLogo from "../account/AccountImportLogo.vue";
 import AccountImportKeystore from "../account/AccountImportKeystore.vue";
+import AccountImportWithProvider from "../account/AccountImportWithProvider.vue";
 
 @Component({
   components: {
     AccountImportKeystore,
-    AccountImportLogo
+    AccountImportLogo,
+    AccountImportWithProvider
   }
 })
 class Init extends Vue {}
