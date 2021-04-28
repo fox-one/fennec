6fe2db0a208bc2e063f95ef6a92e1c4ba9eb38f2
zhangqingwu <18656007202@163.com>
2021-04-28 17:02:13 +0800

    test: add test

---
+++ packages/mixin-extension-dapp/__tests__/index.spec.ts
@@ -0,0 +1,8 @@
+import { Fennec } from "../src";
+
+describe("mixin extension fennec dappf", () => {
+  it("fennec should be init properly", () => {
+    const fennec = new Fennec();
+    expect(fennec.available).toBe(false);
+  });
+});
